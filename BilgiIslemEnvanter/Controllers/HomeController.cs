7d78eeab80b35f5ac743ea53a197f7fb0dbb6506
Memduh Gümen <75331205+adaletinsesi1515@users.noreply.github.com>
2021-03-31 21:00:36 +0300

    DÜZENLEMELER YAPILDI

--- BilgiIslemEnvanter/Controllers/HomeController.cs
+++ BilgiIslemEnvanter/Controllers/HomeController.cs
@@ -3,14 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BilgiIslemEnvanter.Models.Entity;
 
 namespace BilgiIslemEnvanter.Controllers
 {
     public class HomeController : Controller
     {
+        private BilgiIslemEntities db = new BilgiIslemEntities();
         public ActionResult Index()
         {
-            return View();
+            var liste = db.TonerCikis.ToList();
+            var depopcsayisi = db.Bilgisayarlar.Where(i => i.ZIMMET == false).Count()-1;
+            ViewBag.dgr1 = depopcsayisi;
+            var yazicisayisi = db.Yazicilar.Where(i => i.ZIMMET == false).Count()-1;
+            ViewBag.dgr2 = yazicisayisi;
+            var tarayicisayisi = db.Tarayicilar.Where(i => i.ZIMMET == false).Count()-1;
+            ViewBag.dgr3 = tarayicisayisi;
+            var perssayisi = db.Personeller.Where(i => i.DURUM== true).Count();
+            ViewBag.dgr4 = perssayisi;
+
+
+
+            return View(liste);
+
+
+            
+           
         }
 
         public ActionResult About()
