dbcb3b5dcfc9de1988ae56c27e50714f3c0cad20
Memduh Gümen <75331205+adaletinsesi1515@users.noreply.github.com>
2021-03-31 17:25:29 +0300

    bilgi güncelleme

--- BilgiIslemEnvanter/Models/Entity/Model1.Designer.cs
+++ BilgiIslemEnvanter/Models/Entity/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Burdur\Desktop\BilgiIslemEnvanter\BilgiIslemEnvanter\Models\Entity\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\ab96085\Source\Repos\BilgiIslemEnvanters\BilgiIslemEnvanter\Models\Entity\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
